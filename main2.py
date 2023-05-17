67b44c7001441c67c5e022c1350a714b462dc2eb
Aadyant Maity <aadyantmaity@users.noreply.github.com>
2023-05-17 00:15:32 -0700

    linear and forest

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import MinMaxScaler
 
@@ -37,7 +37,7 @@ def main():
 
     train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(inputs, outputs, test_size=0.2, random_state=103)
 
-    model = LinearRegression()
+    model = RandomForestRegressor()
     model.fit(train_inputs, train_outputs)
 
     predictions = model.predict(test_inputs)
