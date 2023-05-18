c3fe1e035dae436d1087597f9b5c1c1366eaaf49
Aadyant Maity <aadyantmaity@users.noreply.github.com>
2023-05-18 13:23:26 -0700

    added hyperparameters

--- main.py
+++ main.py
@@ -16,18 +16,22 @@ def perform_regression(model):
     inputs = data[:, :-1]
     outputs = data[:, -1]
 
-    train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(inputs, outputs, test_size=0.2, random_state=103)
+    train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(inputs, outputs, test_size=0.1, random_state=103)
 
-    model.fit(train_inputs, train_outputs)
+    scaler = MinMaxScaler()
+    train_inputs_scaled = scaler.fit_transform(train_inputs)
+    test_inputs_scaled = scaler.transform(test_inputs)
 
-    predictions = model.predict(test_inputs)
+    model.fit(train_inputs_scaled, train_outputs)
+
+    predictions = model.predict(test_inputs_scaled)
 
     r2 = r2_score(test_outputs, predictions)
     print(model.__class__.__name__, "R^2 Score:", r2)
 
 if __name__ == '__main__':
-    linear_regression_model = LinearRegression()
-    random_forest_model = RandomForestRegressor()
+    linear_regression_model = LinearRegression(fit_intercept=True)
+    random_forest_model = RandomForestRegressor(n_estimators=100, max_depth=None, min_samples_split=3)
 
     perform_regression(linear_regression_model)
     perform_regression(random_forest_model)
