8fd75a37f7201261f7f85e4f3d5d8850d45c2cbe
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-05-23 21:34:01 -0700

    Remove excessive scaling, rename variables, made path relative to current script

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 from utils import preprocess
 from pathlib import Path
-import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import MinMaxScaler
 
 def perform_regression(model):
-    current_dir = Path.cwd()
-    filename = current_dir / 'GLODAPDATA.csv'
+    current_dir = Path(__file__).parent
+    filename = current_dir / 'GLODAPv2.2022_Atlantic_Ocean.csv'
     data = preprocess(filename)
 
     inputs = data[:, :-1]
     outputs = data[:, -1]
 
-    train_inputs, test_inputs, train_outputs, test_outputs = train_test_split(inputs, outputs, test_size=0.1, random_state=103)
+    train_inputs, dev_inputs, train_targets, dev_targets = train_test_split(inputs, outputs, test_size=0.1, random_state=103)
 
-    scaler = MinMaxScaler()
-    train_inputs_scaled = scaler.fit_transform(train_inputs)
-    test_inputs_scaled = scaler.transform(test_inputs)
+    model.fit(train_inputs, train_targets)
 
-    model.fit(train_inputs_scaled, train_outputs)
+    predictions = model.predict(dev_inputs)
 
-    predictions = model.predict(test_inputs_scaled)
-
-    r2 = r2_score(test_outputs, predictions)
+    r2 = r2_score(dev_targets, predictions)
     print(model.__class__.__name__, "R^2 Score:", r2)
 
 if __name__ == '__main__':
