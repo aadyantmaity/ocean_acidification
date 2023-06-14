d2aa8aba69a57c8457b4703e572494b671c6b2fe
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2023-06-14 00:04:26 -0500

    Iterate over N for top N features as opposed to hard coding it

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def train_model(model, train_inputs, train_targets):
 def evaluate_model(model, inputs, targets, model_name):
     predictions = model.predict(inputs)
     r2 = r2_score(targets, predictions)
-    print(model_name, "R^2 Score:", r2)
+    print(model_name, 'R^2 Score:', r2)
     return r2
 
 def plot_loss_curve(model, model_name):
@@ -73,14 +73,10 @@ def plot_feature_importance(model, train_inputs, train_targets, feature_names):
         plt.tight_layout()
         plt.show()
 
-def get_top_N_features(importances, N):
-    return np.argsort(importances)[-N:]
-
 if __name__ == '__main__':
     current_dir = Path(__file__).parent
     filename = current_dir / 'GLODAPv2.2022_Atlantic_Ocean.csv'
-    feature_names = ["G2Temperature", "G2 Salinity", "G2Oxygen", "G2aou", "G2talk", "G2cfc11", "G2cfc12", "G2phosphate", "G2pcfc12", "G2nitrate", "G2silicate"]
-    N = 3
+    feature_names = ['G2Temperature', 'G2 Salinity', 'G2Oxygen', 'G2aou', 'G2talk', 'G2cfc11', 'G2cfc12', 'G2phosphate', 'G2pcfc12', 'G2nitrate', 'G2silicate']
 
     inputs, outputs = preprocess_data(filename)
     train_inputs, dev_inputs, train_targets, dev_targets = split_data(inputs, outputs)
@@ -90,30 +86,33 @@ if __name__ == '__main__':
     svm_model = SVR(kernel='linear')
     mlp_model = MLPRegressor(hidden_layer_sizes=(25, 50, 10), batch_size=64, learning_rate_init=0.001, max_iter=200, random_state=0)
 
-    models = [linear_regression_model, random_forest_regressor, svm_model, mlp_model]
-    model_names = ["Linear Regression", "Random Forest Regressor", "Support Vector Machine", "MLP Regressor"]
+    models = {
+        # 'Linear Regression': linear_regression_model,
+        'Random Forest Regressor': random_forest_regressor,
+        # 'Support Vector Machine': svm_model,
+        # 'MLP Regressor': mlp_model,
+    }
     train_scores = []
     dev_scores = []
 
     random_forest_regressor.fit(train_inputs, train_targets)
     importances = random_forest_regressor.feature_importances_
-    top_features_indices = get_top_N_features(importances, N)
-    top_train_inputs = train_inputs[:, top_features_indices]
-    top_dev_inputs = dev_inputs[:, top_features_indices]
+    top_features_indices = np.argsort(importances)
 
-    for model, model_name in zip(models, model_names):
-        if isinstance(model, RandomForestRegressor):
+    for model, model_name in models.items():
+
+        # Loop over the number of "most important feature" to include
+        for N in range(3, importances.shape[0]):
+            top_train_inputs = train_inputs[:, top_features_indices[-N:]]
+            top_dev_inputs = dev_inputs[:, top_features_indices[-N:]]
             train_model(model, top_train_inputs, train_targets)
             train_score = evaluate_model(model, top_train_inputs, train_targets, model_name)
             dev_score = evaluate_model(model, top_dev_inputs, dev_targets, model_name)
-        else:
-            train_model(model, top_train_inputs, train_targets)
-            train_score = evaluate_model(model, top_train_inputs, train_targets, model_name)
-            dev_score = evaluate_model(model, top_dev_inputs, dev_targets, model_name)
 
-        train_scores.append(train_score)
-        dev_scores.append(dev_score)
-        plot_loss_curve(model, model_name)
+            train_scores.append(train_score)
+            dev_scores.append(dev_score)
+            plot_loss_curve(model, model_name)
+            # plot the curve with number of top N features on x-axis and performance on y axis
 
-    plot_model_performance(model_names, train_scores, dev_scores)
+    plot_model_performance(models.keys(), train_scores, dev_scores)
     plot_feature_importance(random_forest_regressor, train_inputs, train_targets, feature_names)
